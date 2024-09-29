f1f0bbec6d79457f23078cdb624400f769701631
Cao Văn Thiện <caovanthien09102002@gmail.com>
2024-09-29 23:55:57 +0700

    second commit

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const mongoose = require("mongoose");
 const { MGDB_USERNAME, MGDB_PASSWORD, MGDB_IP, MGDB_PORT } = require("./config/config");
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World!!!");
 });
+
 const connectWithRetry = () => {
     mongoose
         .connect(`mongodb://${MGDB_USERNAME}:${MGDB_PASSWORD}@${MGDB_IP}:${MGDB_PORT}`)
         .then(() => console.log("MongoDB Connected."))
         .catch((err) => {console.log("Error Connect!"); setTimeout(connectWithRetry, 5000)});
 };
+console.log(`mongodb://${MGDB_USERNAME}:${MGDB_PASSWORD}@${MGDB_IP}:${MGDB_PORT}`)
 connectWithRetry();
 
 const port = process.env.PORT || 3001;
