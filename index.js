0dc35b945810f4e126c545b95c7514c4960fcfd8
Cao Văn Thiện <caovanthien09102002@gmail.com>
2024-09-30 22:41:03 +0700

    CICD

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+app.use(express.json());
+
 const { MGDB_USERNAME, MGDB_PASSWORD, MGDB_IP, MGDB_PORT } = require("./config/config");
 
 app.get("/", (req, res) => {
@@ -13,8 +15,35 @@ const connectWithRetry = () => {
         .then(() => console.log("MongoDB Connected."))
         .catch((err) => {console.log("Error Connect!"); setTimeout(connectWithRetry, 5000)});
 };
-console.log(`mongodb://${MGDB_USERNAME}:${MGDB_PASSWORD}@${MGDB_IP}:${MGDB_PORT}`)
+console.log(`mongodb://${MGDB_USERNAME}:${MGDB_PASSWORD}@${MGDB_IP}:${MGDB_PORT}/myDB`)
 connectWithRetry();
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.post("/add-user", async (req, res) => {
+    try {
+        console.log(req);
+        const { name, email } = req.body;
+        const newUser = new User({ name, email });
+        await newUser.save();
+        res.status(201).json({ message: "User added successfully", user: newUser });
+    } catch (error) {
+        res.status(500).json({ message: "Error adding user", error });
+    }
+});
+
+app.get("/users", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching users", error });
+    }
+});
 
 const port = process.env.PORT || 3001;
 
